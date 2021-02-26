9d518e34bb9c235aca2af685539674ee9b76917e
Felipe Pedrosa <felipeepedrosa@gmail.com>
2021-02-25 21:27:32 -0300

    Added project files
      - Files created by https://start.spring.io

---
+++ src/main/java/br/com/felipepedrosa/springuploadexemple/SpringUploadExempleApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipepedrosa.springuploadexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUploadExempleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUploadExempleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/felipepedrosa/springuploadexemple/SpringUploadExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipepedrosa.springuploadexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUploadExempleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
